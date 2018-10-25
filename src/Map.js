05f41550100a327d64f42b5f751b8412317025d3
Appelbaumgl <Appelbaumgl@msoe.edu>
2018-10-24 21:37:17 -0500

    Initial Commit

---
+++ src/Map.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render() {
+      return (
+        <div id="map" className="Map"></div>
+      );
+    }
+  }
+  
+  export default Map;
+  
