fb6228813d71560f0a53283bb0c5e63c7e644afa
Appelbaumgl <Appelbaumgl@msoe.edu>
2018-10-27 15:52:21 -0500

    Somewhat messy, but working example.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import Map from './Map'
-import './App.css';
+import React, { Component } from 'react'
+import { Map, TileLayer } from 'react-leaflet'
 
 class App extends Component {
+  state = {
+    lat: 43.0440,
+    lng: -87.9084,
+    zoom: 15,
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng]
     return (
-      <div className="App">
-        <Map>
-        </Map>
-      </div>
+      <Map key={'map'} center={position} zoom={this.state.zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
     );
   }
 }
