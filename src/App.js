4b12ca86306ff411ae172adc224c807adce35fba
Appelbaumgl <Appelbaumgl@msoe.edu>
2018-10-27 20:23:01 -0500

    Added a component to change the marker size for demo and example purposes.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+import { Map, TileLayer, Marker } from 'react-leaflet'
+import MarkerMover from './MarkerMover'
 
 class App extends Component {
-  state = {
-    lat: 43.0440,
-    lng: -87.9084,
-    zoom: 15,
-  }
+    state = {
+        lat: 43.0440,
+        lng: -87.9084,
+        zoom: 15,
+        marker: {
+            lat: 43.0440,
+            lng: -87.9084
+        }
+    }
 
-  render() {
-    const position = [this.state.lat, this.state.lng]
-    return (
-      <Map key={'map'} center={position} zoom={this.state.zoom}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </Map>
-    );
-  }
+    constructor() {
+        super();
+        this.updateMarker = this.updateMarker.bind(this);
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng]
+        var driverIcon = L.icon({
+            iconUrl: 'https://png.icons8.com/metro/1600/car.png',
+            iconSize: [30,30],
+            iconAnchor: [15,15],
+            popupAnchor: [15,15]
+        });
+
+        return (
+            <div>
+                <MarkerMover updateMarker={this.updateMarker}/>
+                <Map attributionControl={false} center={position} zoom={this.state.zoom}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <Marker position={this.state.marker} icon={driverIcon}/>
+                </Map>
+            </div>
+        );
+    }
+
+    updateMarker(marker) {
+        this.setState({marker: marker});
+    }
 }
 
 export default App;
---
+++ src/MarkerMover.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class MarkerMover extends Component {
+
+    state = {}
+
+    constructor() {
+        super();
+        this.state.Latitude = '';
+        this.state.Longitude = '';
+    }
+
+    render() {
+        return (
+            <div>
+                <label> Latitude: </label>
+                <input type='number' value={this.state.Latitude} onChange={(event) => this.handleChange('Latitude', event)}/>
+                <label> Longitude: </label>
+                <input type='number' value={this.state.Longitude} onChange={(event) => this.handleChange('Longitude', event)}/>
+                <button onClick={ () => { this.handleSubmit() } }>Submit</button>
+            </div>
+        );
+    }
+
+    handleChange(field, event) {
+        if (field === 'Latitude') {
+            this.setState({ Latitude: event.target.value });
+        } else if (field === 'Longitude') {
+            this.setState({ Longitude: event.target.value });
+        }
+    }
+
+    handleSubmit() {
+        var lat = Number.parseFloat(this.state.Latitude);
+        var lng = Number.parseFloat(this.state.Longitude);
+        if (isNaN(lat) || isNaN(lng)) {
+            return;
+        } else {
+            this.props.updateMarker({ lat: lat, lng: lng });
+        }
+    }
+}
+
+export default MarkerMover;
